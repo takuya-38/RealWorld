21376f66ac716f3c4aaa06771f30c31df4e16f6d
takuya-38 <takuya.kogoma.38@gmail.com>
2024-02-18 23:30:04 +0900

    change: httpsへのリダイレクトを有効

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_sslはHTTPをHTTPSにリダイレクトさせる
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
