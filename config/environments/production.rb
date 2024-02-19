fde021866b17e7f344da07941e2a2204f082fdd7
takuya-38 <takuya.kogoma.38@gmail.com>
2024-02-19 15:27:54 +0900

    fix: ssl化をoff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_sslはHTTPをHTTPSにリダイレクトさせる
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
